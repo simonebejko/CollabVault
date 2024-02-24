585f47cd80edd316b32f795bed893736355ea628
Simone <simonebejko@outlook.com>
2024-02-24 16:57:33 -0500

    Moved to Django 4.2

--- src/collabvault/wsgi.py
+++ src/collabvault/wsgi.py
@@ -1,14 +1,16 @@
 """
-Exposes the WSGI callable as a module-level variable named ``application``.
+WSGI config for collabvault project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "collabvault.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collabvault.settings')
 
 application = get_wsgi_application()
