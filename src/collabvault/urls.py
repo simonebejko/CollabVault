9c5b6fa60f0cc865399fbd4851e42b04b23ddb66
Simone <simonebejko@outlook.com>
2024-02-27 17:15:51 -0500

    Add activate/deactivate project views

--- src/collabvault/urls.py
+++ src/collabvault/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path
 
 from landing import views as landing_views
+from projects import views as project_views 
 
 urlpatterns = [
     path('', landing_views.home_page_view),
+    path('activate/project/<slug:handle>', project_views.activate_project_view),
+    path('deactivate/project/<slug:handle>', project_views.deactivate_project_view),
     path('admin/', admin.site.urls),
 ]
--- src/landing/views.py
+++ src/landing/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page_view(request):
+    print(request.session.get('project_handle'))
     return render(request, "landing/home.html", {})
