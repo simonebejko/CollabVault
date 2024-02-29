eca5636d8d4bd79fdbd5b8b53ce609836870e7c1
Simone <simonebejko@outlook.com>
2024-02-28 19:59:25 -0500

    Implement views for item list and item details

--- src/items/urls.py
+++ src/items/urls.py
@@ -19,5 +19,7 @@ from . import views
 
 app_name='items'
 urlpatterns = [
-    path("create/", views.item_create_view, name='create')
+    path("create/", views.item_create_view, name='create'),
+    path("", views.item_list_view, name='list'),
+    path("<int:id>/", views.item_detail_view, name='detail')
 ]
