ae5761fe4510d60ae04e7196e66104b5ff2a3f18
Simone <simonebejko@outlook.com>
2024-03-03 15:20:07 -0500

    Render and handle forms with htmx, implement refresh trigger event

--- src/items/views.py
+++ src/items/views.py
@@ -4,6 +4,10 @@ from . import forms
 from .models import Item
 from projects import cache as projects_cache
 from projects.decorators import project_required
+from django.urls import reverse
+from django_htmx.http import HttpResponseClientRefresh, HttpResponseClientRedirect
+from django.http import HttpResponse
+from django_htmx.http import trigger_client_event
 
 # Create your views here.
 @project_required
@@ -43,15 +47,24 @@ def item_delete_view(request, id=None):
 @project_required
 @login_required
 def item_create_view(request):
+    template_name = 'items/create.html'
+    if request.htmx:
+        template_name = 'items/snippets/form.html'
     form = forms.ItemCreateForm(request.POST or None)
     if form.is_valid():
         item_obj = form.save(commit=False)
         item_obj.project = request.project
         item_obj.added_by = request.user
         item_obj.save()
-        projects_cache.get_user_projects(request.user.username)
+        if request.htmx:
+            custom_refresh_event = "refresh-list-view"
+            response = HttpResponse("")
+            return trigger_client_event(response, custom_refresh_event)
         return redirect(item_obj.get_absolute_url())
+    action_create_url = reverse("items:create")
     context = {
-        "form": form
+        "form": form,
+        "btn_label": "Create item",
+        "action_url": action_create_url
     }
-    return render(request, 'items/create.html', context)
+    return render(request, template_name, context)
